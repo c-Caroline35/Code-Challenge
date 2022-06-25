4136f705b0c5c2a8206259ec0e40e7656b249bb7
c-Caroline35 <104658845+c-Caroline35@users.noreply.github.com>
2022-06-25 11:31:48 -0500

    Create Submission.js

---
+++ Submission.js
@@ -0,0 +1,26 @@
+const findSum = function(array) {
+  let sum = 0;
+  for(let a of array)
+    sum += a; 
+  return sum; 
+};
+
+const findFrequency = function(array) {
+  // your code here - don't forget to return an object!
+};
+
+const isPalindrome = function(str) {
+  // your code here - don't forget to return a boolean!
+};
+
+const largestPair = function(array) {
+  // your code here - don't forget to return a number!
+};
+
+const removeParenth = function(str) {
+  // your code here - don't forget to return a string!
+};
+
+const scoreScrabble = function(str) {
+  // your code here - don't forget to return a number!
+};
